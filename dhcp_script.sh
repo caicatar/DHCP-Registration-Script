edd5581fb6df81d4c3bc3706d81707f35e6403e5
CA Icatar <themadgarlic@gmail.com>
2021-09-15 14:24:46 +0800

    update 2.0
    ~ Added Clear dnsmasq.lease option
    ~ Changed requirement format and # to start of each line in hosts
    ~ Added validation for ethers when registering devices
    ~ Added exit option for menu

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -11,6 +11,8 @@ echo  "                              3. Clear IP"
 echo  "                              4. View Logs"
 echo  "                              5. Clear Logs"
 echo  "                              6. Clear dnsmasq.leases"
+echo  "                              7. Restart dnsmasq"
+echo  "                              0. Exit"
 echo "Select:"
 read option
 
@@ -23,20 +25,22 @@ case $option in
     read macaddress
  
     #checks for available IP and provides line
-    linenum=$(grep -n '#OPEN' hosts | gawk '{print $1}' FS=":" | head -1)
+    linenum=$(grep -n -s '#OPEN' hosts | gawk '{print $1}' FS=":" | head -1 )  
     #get ip from that available IP 
-    getip=$(sed -n ${linenum}p hosts | awk -F '   '  '{print $1}')
+    getip=$(sed -n ${linenum}p hosts | awk -F '   '  '{print $1}' | cut -c2- | xargs )
     #get line from ethers that matches the available IP from hosts 
-    linenumeth=$(grep -n ${getip} ethers | gawk '{print $1}' FS=":" | head -1)
+    linenumeth=$(grep -n -s ${getip} ethers | gawk '{print $1}' FS=":" | head -1) 
+
 
     #capture dhcp and mac name into variables
     dhcp=$dhcpname
     mac=$macaddress
 
     #check if the are available IP in hosts file then modify and display results
-    if [[ $linenum -ge  1 ]] 
+    if [[ $linenum -ge  1 && $linenumeth -ge 1 ]] 
     then 
          sed -i "${linenum}s/#OPEN/${dhcp}/" hosts
+         sed -i "${linenum}s/#//" hosts
          sed -i "${linenumeth}s/#OPEN/${mac}/" ethers
          lineresult=$(sed -n ${linenum}p hosts)
          ethresult=$(sed -n ${linenumeth}p ethers)
@@ -51,47 +55,55 @@ case $option in
          echo "Ethers:" $ethresult
          echo "-------------------------------------------------"
      else
-         echo "--------------------------------------------------"
-         echo "|No available IPs found, please check /etc/hosts |"
-         echo "--------------------------------------------------"
-     fi
+         echo "-----------------------------------------------------------------"
+         echo "|No available IPs found, please check /etc/hosts or /etc/ethers |"
+         echo "-----------------------------------------------------------------"
+       fi
    ;;
     
    2)
         echo "------------------------------------------------"
         echo "|Available IPs:                                |"
         echo "------------------------------------------------"
-	grep -n '#OPEN' hosts | gawk '{print $1}' FS="  "
+	grep -n '#OPEN' hosts | gawk '{print $1}' FS="  " 
    ;;
 
   3)
-	echo "Clear IP"
+	echo "--------------------------------------------------" 
+        echo "|Clear IP                                        |"
+        echo "--------------------------------------------------"
         echo "Search IP:"
         read clearip
-        echo "Clear IP? Y/N"
+        echo "Clear IP?" $clearip "(Y/N)"
 	read sel
 	case $sel in 
                 Y)
         	ipline=$(grep -n ${clearip} hosts | gawk '{print $1}' FS=":" | head -1)
    		iplineeth=$(grep -n ${clearip} ethers | gawk '{print $1}' FS=":" | head -1)
+                ipcheck=$(sed -n ${ipline}p hosts | cut -c1-13 | awk '{$1=$1;print}')
 
+                if [[ $clearip == $ipcheck ]]
+                then
           	hostrep=$(sed -n ${ipline}p hosts | cut -c 17-)
-                ethrep=$(sed -n ${iplineeth}p ethers | rev | cut -c14- | rev)
-	
+                ethrep=$(sed -n ${iplineeth}p ethers | rev | cut -c14- | rev )
+
         	sed -i "${ipline}s/${hostrep}/#OPEN/" hosts
+                sed -i "/^${clearip}/ s/./#&/" hosts
        	        sed -i "${iplineeth}s/${ethrep}/#OPEN/" ethers
 
          	echo "Cleared:" $clearip "-" $hostrep "-" $ethrep "|" $now  >>  dhcp_logs
          	echo "--------------------------------------------------" 
-         	echo "|Successfully cleared IP:                     |"
+         	echo "|Successfully cleared IP:                        |"
          	echo "--------------------------------------------------"
         	 #display results for host and ethers ( check if matches )    
         	 echo "Hosts:" $clearip
         	 echo "-------------------------------------------------"
-
+                else    
+                     echo "IP not found!"
+                fi
 	        ;;
                 N)
-                echo "Yahahar"
+                exit
                 ;;
                 esac
                
@@ -105,11 +117,34 @@ case $option in
  ;;
 
  5)
-        cat /dev/null > dhcp_logs
-        echo "Cleared Logs"
+       echo "------------------------------------------------"
+       echo "|Clear Logs:                                   |"
+       echo "------------------------------------------------"
+       echo "Clear logs? (Y/N)"
+       read sel
+	case $sel in 
+        Y)
+                cat /dev/null > dhcp_logs
+                echo "Cleared Logs"
+	;;
+        N)
+                exit
+        ;;
+        esac
  ;;
 
- 6)     echo "Cleared dnsmasq.leases"
+ 6)     
+        systemctl stop dnsmasq
+        cat /dev/null > var/lib/dnsmasq/dnsmasq.leases
+        systemctl start dnsmasq
+        
+        echo "Cleared dnsmasq.leases and restarted dnsmasq service"
+ ;;
+
+
+ 7)     
+        systemctl restart dnsmasq
+        echo "Restarted dnsmasq service"
  ;;
 
   0)

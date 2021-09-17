bfc7f23f8bbcffc5046918da158379877da56123
CA Icatar <themadgarlic@gmail.com>
2021-09-17 16:32:37 +0800

    Updates
    ~ Added MAC address validations
    ~ Added saving condition on registration

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -7,7 +7,7 @@ echo "[- Please make sure that criterias for open IPs are matched to avoid error
 echo "                              [- Choose operation: ]"
 echo  "                              1. Register device"
 echo  "                              2. Check available IP"
-echo  "                              3. Clear IP"
+echo  "                              3. Delete Host/IP"
 echo  "                              4. View Logs"
 echo  "                              5. Clear Logs"
 echo  "                              6. Clear dnsmasq.leases"
@@ -19,46 +19,64 @@ read option
 case $option in 
 
    1)
-    echo "Enter dhcp name ( no spaces ):" 
+    echo "Enter host name ( no spaces ):" 
     read dhcpname
     echo "Enter mac address: ( correct format no spaces ):"
     read macaddress
- 
-    #checks for available IP and provides line
-    linenum=$(grep -n -s '#OPEN' hosts | gawk '{print $1}' FS=":" | head -1 )  
-    #get ip from that available IP 
-    getip=$(sed -n ${linenum}p hosts | awk -F '   '  '{print $1}' | cut -c2- | xargs )
-    #get line from ethers that matches the available IP from hosts 
-    linenumeth=$(grep -n -s ${getip} ethers | gawk '{print $1}' FS=":" | head -1) 
+    #validate Mac address format
+    format1=$(echo $macaddress | sed "/^\([0-9Aa-Zz][0-9Aa-Zz]:\)\{5\}[0-9Aa-Zz][0-9A-Za-z]$/p" )
+    format2=$(echo $format1 | rev | cut -c18- | rev | xargs)
+    if [[ $macaddress == $format2 ]]
+    then
+    echo "Save device? Y/N"
+    read choice
+        case $choice in
+                Y)
+                   
+                   #checks for available IP and provides line
+                   linenum=$(grep -n -s '#OPEN' hosts | gawk '{print $1}' FS=":" | head -1 )  
+                   #get ip from that available IP 
+                   getip=$(sed -n ${linenum}p hosts | awk -F '   '  '{print $1}' | cut -c2- | xargs )
+                   #get line from ethers that matches the available IP from hosts 
+                   linenumeth=$(grep -n -s ${getip} ethers | gawk '{print $1}' FS=":" | head -1) 
 
+                    #capture dhcp and mac name into variables
+                    dhcp=$dhcpname
+                    mac=$macaddress
 
-    #capture dhcp and mac name into variables
-    dhcp=$dhcpname
-    mac=$macaddress
-
-    #check if the are available IP in hosts file then modify and display results
-    if [[ $linenum -ge  1 && $linenumeth -ge 1 ]] 
-    then 
-         sed -i "${linenum}s/#OPEN/${dhcp}/" hosts
-         sed -i "${linenum}s/#//" hosts
-         sed -i "${linenumeth}s/#OPEN/${mac}/" ethers
-         lineresult=$(sed -n ${linenum}p hosts)
-         ethresult=$(sed -n ${linenumeth}p ethers)
-         systemctl restart dnsmasq
-         echo "Registered:" $lineresult "-" $macaddress "|" $now  >>  dhcp_logs
-         echo "--------------------------------------------------" 
-         echo "|Successfully registered IP:                     |"
-         echo "--------------------------------------------------"
-         #display results for host and ethers ( check if matches )    
-         echo "Hosts:" $lineresult
-         echo "-------------------------------------------------"
-         echo "Ethers:" $ethresult
-         echo "-------------------------------------------------"
-     else
-         echo "-----------------------------------------------------------------"
-         echo "|No available IPs found, please check /etc/hosts or /etc/ethers |"
-         echo "-----------------------------------------------------------------"
-       fi
+                   #check if the are available IP in hosts file then modify and display results
+                   if [[ $linenum -ge  1 && $linenumeth -ge 1 ]] 
+                   then 
+                   sed -i "${linenum}s/#OPEN/${dhcp}/" hosts
+                   sed -i "${linenum}s/#//" hosts
+                   sed -i "${linenumeth}s/#OPEN/${mac}/" ethers
+                   lineresult=$(sed -n ${linenum}p hosts)
+                   ethresult=$(sed -n ${linenumeth}p ethers)
+                   systemctl restart dnsmasq
+                   echo "Registered:" $lineresult "-" $macaddress "|" $now  >>  dhcp_logs
+                   echo "--------------------------------------------------" 
+                   echo "|Successfully registered IP:                     |"
+                   echo "--------------------------------------------------"
+                   #display results for host and ethers ( check if matches )    
+                   echo "Hosts:" $lineresult
+                   echo "-------------------------------------------------"
+                   echo "Ethers:" $ethresult
+                   echo "-------------------------------------------------"
+                else
+                   echo "-----------------------------------------------------------------"
+                   echo "|No available IPs found, please check /etc/hosts or /etc/ethers |"
+                   echo "-----------------------------------------------------------------"
+                fi
+                ;;
+                N)
+                   exit
+                ;;
+                esac
+   else
+        echo "-----------------------------------------------------------------"
+        echo "| Invalid MAC address format                                    |"
+        echo "-----------------------------------------------------------------"
+   fi
    ;;
     
    2)
@@ -78,15 +96,19 @@ case $option in
 	read sel
 	case $sel in 
                 Y)
+                #get IP from hosts and ethers
         	ipline=$(grep -n ${clearip} hosts | gawk '{print $1}' FS=":" | head -1)
    		iplineeth=$(grep -n ${clearip} ethers | gawk '{print $1}' FS=":" | head -1)
+                #get IP from $ipline
                 ipcheck=$(sed -n ${ipline}p hosts | cut -c1-13 | awk '{$1=$1;print}')
 
                 if [[ $clearip == $ipcheck ]]
                 then
+                #get IP from ethers and hosts
           	hostrep=$(sed -n ${ipline}p hosts | cut -c 17-)
-                ethrep=$(sed -n ${iplineeth}p ethers | rev | cut -c14- | rev )
+                ethrep=$(sed -n ${iplineeth}p ethers | rev | cut -c14- | rev | xargs )
 
+                #replace MAC and HOST NAME based on ID with #OPEN
         	sed -i "${ipline}s/${hostrep}/#OPEN/" hosts
                 sed -i "/^${clearip}/ s/./#&/" hosts
        	        sed -i "${iplineeth}s/${ethrep}/#OPEN/" ethers
